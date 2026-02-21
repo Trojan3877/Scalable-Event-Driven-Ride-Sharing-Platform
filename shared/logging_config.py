c2cc7b377236c57003d684f2cb2d4468eb0d98aa
Corey Leath <corey22blue@hotmail.com>
2026-02-21 18:30:23 -0500

    Create shared/logging_config.py

---
+++ shared/logging_config.py
@@ -0,0 +1,18 @@
+import logging
+import sys
+from pythonjsonlogger import jsonlogger
+
+
+def setup_logging():
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+    handler = logging.StreamHandler(sys.stdout)
+    formatter = jsonlogger.JsonFormatter(
+        "%(asctime)s %(levelname)s %(name)s %(message)s"
+    )
+
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    return logger
